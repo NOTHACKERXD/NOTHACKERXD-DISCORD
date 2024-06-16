0acd5fdf6202460ce90c42dcc165aff599c753ae
NOTHACKERXD <158583681+NOTHACKERXD@users.noreply.github.com>
2024-06-16 11:29:59 +0300

    Update Roles.tsx

--- src/components/discord/Roles.tsx
+++ src/components/discord/Roles.tsx
@@ -4,14 +4,19 @@ export default function Roles() {
     <div className="mb-3">
       <h2 className="font-bold text-xs text-slate-300 leading-4 mb-2 uppercase">Roles</h2>
       <div className="flex">
-        
-        
-        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="React" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-orange-400"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">HTML</div></div>
-        </div>
+        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="React"             role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-orange-400">            </span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">HTML</div></div>
         <div className="flex">
-        <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir" role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-violet-700"></span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">CSS</div></div>
+           <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir"           role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-violet-700">            </span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">CSS</div></div>
+          <div className="flex">
+         <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir"           role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-green-700">            </span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">js</div></div>
+        </div>
         
-    </div>
+
+        
+         </div>
+         
+
+       </div>
     </div>
   );
 }
