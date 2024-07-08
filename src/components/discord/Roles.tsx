4e894c02c9ec9def3848934aeec01b198fb384a3
NOTHACKERXD <158583681+NOTHACKERXD@users.noreply.github.com>
2024-07-08 13:01:16 +0300

    Update Roles.tsx

--- src/components/discord/Roles.tsx
+++ src/components/discord/Roles.tsx
@@ -11,7 +11,8 @@ export default function Roles() {
          <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir"           role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-green-700">            </span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">js</div></div>
         <div className="flex">
          <div className="mb-1 mr-1 p-1 font-medium text-xs flex items-center bg-slate-200 dark:bg-zinc-800 rounded" aria-label="Elixir"           role="listitem"><div className="relative items-center"><span className="w-3 h-3 rounded-full mx-1 shrink-0 flex bg-purple-700">            </span></div><div className="max-w-[200px] whitespace-nowrap text-ellipsis mr-1">C</div></div>
-          </div>
+         </div>
+        </div>
         
 
         
