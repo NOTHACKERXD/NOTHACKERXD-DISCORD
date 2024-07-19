7acb419f6ac647a1237f1b89f1d66e6fc54d37f7
NOTHACKERXD <158583681+NOTHACKERXD@users.noreply.github.com>
2024-07-19 14:38:48 +0300

    Update Connections.tsx

--- src/components/discord/Connections.tsx
+++ src/components/discord/Connections.tsx
@@ -8,6 +8,20 @@ import RiotIcon from './icons/RiotIcon';
 export default function Connections() {
   return (
       <div className="flex flex-initial mt-3 flex-col flex-wrap items-stretch">
+
+        {/* instagram */}
+        <div className="h-12 mb-2 text-md w-auto relative flex items-center px-4 justify-between text-white border border-transparent rounded">
+          <div className="flex-row flex text-center items-center whitespace-nowrap text-ellipsis overflow-hidden space-x-1">
+            <InstagramIcon />
+            <span className="text-md font-semibold text-white">nikos_poimenidis_</span>
+            <div className="w-[16px] h-[16px]">
+              <VerifiedIcon />
+            </div>
+          </div>
+          <a href="/instagram"  rel="noopener noreferrer" target="_blank">
+            <AnchorIcon />
+          </a>
+        </div>
         
         {/* GitHub */}
         <div className="h-12 mb-2 text-md w-auto relative flex items-center px-4 justify-between text-white border border-transparent rounded">
@@ -38,19 +52,7 @@ export default function Connections() {
           </a>
         </div>
 
-         {/* instagram */}
-        <div className="h-12 mb-2 text-md w-auto relative flex items-center px-4 justify-between text-white border border-transparent rounded">
-          <div className="flex-row flex text-center items-center whitespace-nowrap text-ellipsis overflow-hidden space-x-1">
-            <InstagramIcon />
-            <span className="text-md font-semibold text-white">nikos_poimenidis_</span>
-            <div className="w-[16px] h-[16px]">
-              <VerifiedIcon />
-            </div>
-          </div>
-          <a href="/instagram"  rel="noopener noreferrer" target="_blank">
-            <AnchorIcon />
-          </a>
-        </div>
+         
         {/* Valorant */}
         {/*
         <div className="h-10 mb-2 text-md w-auto relative flex items-center px-4 justify-between text-white bg-zinc-700 rounded">
